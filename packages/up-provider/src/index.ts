447af37acb9b2804831acd8d028e439f53fdc9cf
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-12-07 10:44:20 -0500

    fix: Switch to using null address

--- packages/up-provider/src/index.ts
+++ packages/up-provider/src/index.ts
@@ -5,12 +5,5 @@ export * from './popup'
 export const isEmptyAccount = (value: `0x${string}` | undefined) => !value
 export const EMPTY_ACCOUNT = '0x0000000000000000000000000000000000000000'
 export const cleanupAccounts = (values: Array<`0x${string}` | undefined>): `0x${string}`[] => {
-  const list = []
-  for (const value of values) {
-    if (isEmptyAccount(value)) {
-      break
-    }
-    list.push(value)
-  }
-  return list as `0x${string}`[]
+  return values?.map(value => (isEmptyAccount(value) ? EMPTY_ACCOUNT : value)) || []
 }
